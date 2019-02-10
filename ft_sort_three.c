3c9bdc51fea4fd51fe2c5cdbb6e3785a28142aa0
Yotta Point <ly.goki@gmail.com>
2019-02-10 08:59:27 +0000

    Add a more efficient way to sort the end, when there are only 3 numbers in one half of the pile.

--- ft_sort_three.c
+++ ft_sort_three.c
@@ -27,7 +27,7 @@ t_pile  *ft_sort_three_0or1(t_pile *pile, t_dope *sol, int pd, int pos)
   return (pile);
 }
 
-t_pile  *ft_sort_three_2(t_pile *pile, t_dope *sol, int pd)
+t_pile  *ft_push2a_thrice(t_pile *pile, t_dope *sol, int pd)
 {
   if (pd == BPILE)
   {
@@ -38,30 +38,40 @@ t_pile  *ft_sort_three_2(t_pile *pile, t_dope *sol, int pd)
   return (pile);
 }
 
+t_pile  *ft_sort_three_loneflag(t_pile *pile, t_dope *sol, int pd, int pos)
+{
+  if (pos == 0)
+    pile = ft_add_ope(sol, pile, RROT * pd);
+  if (pos == 1)
+    pile = ft_add_ope(sol, pile, NROT * pd);
+  pile = ft_push2a_thrice(pile, sol, pd);
+  return (pile);
+}
+
 t_pile  *ft_sort_three(t_pile *pile, t_dope *sol, int pd, int bswap)
 {
   int pos;
   int swap;
   t_lnum  *stack;
+  int   loneflag;
   
   stack = pd == APILE ? pile->a : pile->b;
   swap = pd == APILE ? stack->n > stack->next->n : stack->n < stack->next->n;
   pos = ft_final_pos_last_elem_trio(stack, 
     pd == APILE ? &ft_int_is_bigger : &ft_int_is_smaller);
-  printf("pd is %d, pos is %d, swap is %d, bswap is %d\n", pd, pos, swap, bswap);
-  puts("before sorting");
-  print_pile(*pile);
+  loneflag = ft_length_stack(pile, pd) == 3 ? 1 : 0;
+  swap ^= loneflag && pos == 1 ? 1 : 0; 
   if (swap && bswap)
     pile = ft_add_ope(sol, pile, SS);
   else if (swap)
     pile = ft_add_ope(sol, pile, SWAP * pd);
   else if (bswap)
     pile = ft_add_ope(sol, pile, SWAP * -pd);
+  if (loneflag)
+    return(ft_sort_three_loneflag(pile, sol, pd, pos));
   if (pos == 2)
-    ft_sort_three_2(pile, sol, pd);
+    ft_push2a_thrice(pile, sol, pd);
   else
     ft_sort_three_0or1(pile, sol, pd, pos);
-  puts("after sorting");
-  print_pile(*pile);
   return (pile);
 }
