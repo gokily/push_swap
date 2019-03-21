741b37a93d2cb7984fbe03b55d34fe00e251e845
Goki LY <gly@student.42.fr>
2019-03-21 10:23:10 +0100

    Add a small test suit.

---
+++ test_small_sort_check.sh
@@ -0,0 +1,191 @@
+#!/bin/sh
+
+echo "test 1 * * * *"
+ARG="1" && ./push_swap $ARG | ./checker %ARG
+
+echo "test 2 * * * *"
+ARG="1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2" && ./push_swap $ARG | ./checker %ARG
+
+echo "test 3 * * * *"
+ARG="1 2 3" && ./push_swap $ARG | ./checker %ARG
+ARG="1 3 2" && ./push_swap $ARG | ./checker %ARG
+ARG="2 1 3" && ./push_swap $ARG | ./checker %ARG
+ARG="2 3 1" && ./push_swap $ARG | ./checker %ARG
+ARG="3 1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2 1" && ./push_swap $ARG | ./checker %ARG
+
+echo "test 4 * * * *"
+ARG="1 2 3 4" && ./push_swap $ARG | ./checker %ARG
+ARG="1 2 4 3" && ./push_swap $ARG | ./checker %ARG
+ARG="1 3 2 4" && ./push_swap $ARG | ./checker %ARG
+ARG="1 3 4 2" && ./push_swap $ARG | ./checker %ARG
+ARG="1 4 2 3" && ./push_swap $ARG | ./checker %ARG
+ARG="1 4 3 2" && ./push_swap $ARG | ./checker %ARG
+
+ARG="2 1 3 4" && ./push_swap $ARG | ./checker %ARG
+ARG="2 1 4 3" && ./push_swap $ARG | ./checker %ARG
+ARG="2 3 1 4" && ./push_swap $ARG | ./checker %ARG
+ARG="2 3 4 1" && ./push_swap $ARG | ./checker %ARG
+ARG="2 4 1 3" && ./push_swap $ARG | ./checker %ARG
+ARG="2 4 3 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="3 1 2 4" && ./push_swap $ARG | ./checker %ARG
+ARG="3 1 4 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2 1 4" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2 4 1" && ./push_swap $ARG | ./checker %ARG
+ARG="3 4 1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 4 2 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="4 1 2 3" && ./push_swap $ARG | ./checker %ARG
+ARG="4 1 3 2" && ./push_swap $ARG | ./checker %ARG
+ARG="4 2 1 3" && ./push_swap $ARG | ./checker %ARG
+ARG="4 2 3 1" && ./push_swap $ARG | ./checker %ARG
+ARG="4 3 1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="4 3 2 1" && ./push_swap $ARG | ./checker %ARG
+
+echo "test 5 * * * *"
+ARG="5 1 2 3 4" && ./push_swap $ARG | ./checker %ARG
+ARG="5 1 2 4 3" && ./push_swap $ARG | ./checker %ARG
+ARG="5 1 3 2 4" && ./push_swap $ARG | ./checker %ARG
+ARG="5 1 3 4 2" && ./push_swap $ARG | ./checker %ARG
+ARG="5 1 4 2 3" && ./push_swap $ARG | ./checker %ARG
+ARG="5 1 4 3 2" && ./push_swap $ARG | ./checker %ARG
+
+ARG="5 2 1 3 4" && ./push_swap $ARG | ./checker %ARG
+ARG="5 2 1 4 3" && ./push_swap $ARG | ./checker %ARG
+ARG="5 2 3 1 4" && ./push_swap $ARG | ./checker %ARG
+ARG="5 2 3 4 1" && ./push_swap $ARG | ./checker %ARG
+ARG="5 2 4 1 3" && ./push_swap $ARG | ./checker %ARG
+ARG="5 2 4 3 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="5 3 1 2 4" && ./push_swap $ARG | ./checker %ARG
+ARG="5 3 1 4 2" && ./push_swap $ARG | ./checker %ARG
+ARG="5 3 2 1 4" && ./push_swap $ARG | ./checker %ARG
+ARG="5 3 2 4 1" && ./push_swap $ARG | ./checker %ARG
+ARG="5 3 4 1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="5 3 4 2 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="5 4 1 2 3" && ./push_swap $ARG | ./checker %ARG
+ARG="5 4 1 3 2" && ./push_swap $ARG | ./checker %ARG
+ARG="5 4 2 1 3" && ./push_swap $ARG | ./checker %ARG
+ARG="5 4 2 3 1" && ./push_swap $ARG | ./checker %ARG
+ARG="5 4 3 1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="5 4 3 2 1" && ./push_swap $ARG | ./checker %ARG
+
+echo "test * 5 * * *"
+ARG="1 5 2 3 4" && ./push_swap $ARG | ./checker %ARG
+ARG="1 5 2 4 3" && ./push_swap $ARG | ./checker %ARG
+ARG="1 5 3 2 4" && ./push_swap $ARG | ./checker %ARG
+ARG="1 5 3 4 2" && ./push_swap $ARG | ./checker %ARG
+ARG="1 5 4 2 3" && ./push_swap $ARG | ./checker %ARG
+ARG="1 5 4 3 2" && ./push_swap $ARG | ./checker %ARG
+
+ARG="2 5 1 3 4" && ./push_swap $ARG | ./checker %ARG
+ARG="2 5 1 4 3" && ./push_swap $ARG | ./checker %ARG
+ARG="2 5 3 1 4" && ./push_swap $ARG | ./checker %ARG
+ARG="2 5 3 4 1" && ./push_swap $ARG | ./checker %ARG
+ARG="2 5 4 1 3" && ./push_swap $ARG | ./checker %ARG
+ARG="2 5 4 3 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="3 5 1 2 4" && ./push_swap $ARG | ./checker %ARG
+ARG="3 5 1 4 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 5 2 1 4" && ./push_swap $ARG | ./checker %ARG
+ARG="3 5 2 4 1" && ./push_swap $ARG | ./checker %ARG
+ARG="3 5 4 1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 5 4 2 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="4 5 1 2 3" && ./push_swap $ARG | ./checker %ARG
+ARG="4 5 1 3 2" && ./push_swap $ARG | ./checker %ARG
+ARG="4 5 2 1 3" && ./push_swap $ARG | ./checker %ARG
+ARG="4 5 2 3 1" && ./push_swap $ARG | ./checker %ARG
+ARG="4 5 3 1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="4 5 3 2 1" && ./push_swap $ARG | ./checker %ARG
+
+echo "test * * 5 * *"
+ARG="1 2 5 3 4" && ./push_swap $ARG | ./checker %ARG
+ARG="1 2 5 4 3" && ./push_swap $ARG | ./checker %ARG
+ARG="1 3 5 2 4" && ./push_swap $ARG | ./checker %ARG
+ARG="1 3 5 4 2" && ./push_swap $ARG | ./checker %ARG
+ARG="1 4 5 2 3" && ./push_swap $ARG | ./checker %ARG
+ARG="1 4 5 3 2" && ./push_swap $ARG | ./checker %ARG
+
+ARG="2 1 5 3 4" && ./push_swap $ARG | ./checker %ARG
+ARG="2 1 5 4 3" && ./push_swap $ARG | ./checker %ARG
+ARG="2 3 5 1 4" && ./push_swap $ARG | ./checker %ARG
+ARG="2 3 5 4 1" && ./push_swap $ARG | ./checker %ARG
+ARG="2 4 5 1 3" && ./push_swap $ARG | ./checker %ARG
+ARG="2 4 5 3 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="3 1 5 2 4" && ./push_swap $ARG | ./checker %ARG
+ARG="3 1 5 4 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2 5 1 4" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2 5 4 1" && ./push_swap $ARG | ./checker %ARG
+ARG="3 4 5 1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 4 5 2 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="4 1 5 2 3" && ./push_swap $ARG | ./checker %ARG
+ARG="4 1 5 3 2" && ./push_swap $ARG | ./checker %ARG
+ARG="4 2 5 1 3" && ./push_swap $ARG | ./checker %ARG
+ARG="4 2 5 3 1" && ./push_swap $ARG | ./checker %ARG
+ARG="4 3 5 1 2" && ./push_swap $ARG | ./checker %ARG
+ARG="4 3 5 2 1" && ./push_swap $ARG | ./checker %ARG
+
+echo "test * * * 5 *"
+ARG="1 2 3 5 4" && ./push_swap $ARG | ./checker %ARG
+ARG="1 2 4 5 3" && ./push_swap $ARG | ./checker %ARG
+ARG="1 3 2 5 4" && ./push_swap $ARG | ./checker %ARG
+ARG="1 3 4 5 2" && ./push_swap $ARG | ./checker %ARG
+ARG="1 4 2 5 3" && ./push_swap $ARG | ./checker %ARG
+ARG="1 4 3 5 2" && ./push_swap $ARG | ./checker %ARG
+
+ARG="2 1 3 5 4" && ./push_swap $ARG | ./checker %ARG
+ARG="2 1 4 5 3" && ./push_swap $ARG | ./checker %ARG
+ARG="2 3 1 5 4" && ./push_swap $ARG | ./checker %ARG
+ARG="2 3 4 5 1" && ./push_swap $ARG | ./checker %ARG
+ARG="2 4 1 5 3" && ./push_swap $ARG | ./checker %ARG
+ARG="2 4 3 5 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="3 1 2 5 4" && ./push_swap $ARG | ./checker %ARG
+ARG="3 1 4 5 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2 1 5 4" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2 4 5 1" && ./push_swap $ARG | ./checker %ARG
+ARG="3 4 1 5 2" && ./push_swap $ARG | ./checker %ARG
+ARG="3 4 2 5 1" && ./push_swap $ARG | ./checker %ARG
+
+ARG="4 1 2 5 3" && ./push_swap $ARG | ./checker %ARG
+ARG="4 1 3 5 2" && ./push_swap $ARG | ./checker %ARG
+ARG="4 2 1 5 3" && ./push_swap $ARG | ./checker %ARG
+ARG="4 2 3 5 1" && ./push_swap $ARG | ./checker %ARG
+ARG="4 3 1 5 2" && ./push_swap $ARG | ./checker %ARG
+ARG="4 3 2 5 1" && ./push_swap $ARG | ./checker %ARG
+
+echo "test * * * * 5"
+ARG="1 2 3 4 5" && ./push_swap $ARG | ./checker %ARG
+ARG="1 2 4 3 5" && ./push_swap $ARG | ./checker %ARG
+ARG="1 3 2 4 5" && ./push_swap $ARG | ./checker %ARG
+ARG="1 3 4 2 5" && ./push_swap $ARG | ./checker %ARG
+ARG="1 4 2 3 5" && ./push_swap $ARG | ./checker %ARG
+ARG="1 4 3 2 5" && ./push_swap $ARG | ./checker %ARG
+
+ARG="2 1 3 4 5" && ./push_swap $ARG | ./checker %ARG
+ARG="2 1 4 3 5" && ./push_swap $ARG | ./checker %ARG
+ARG="2 3 1 4 5" && ./push_swap $ARG | ./checker %ARG
+ARG="2 3 4 1 5" && ./push_swap $ARG | ./checker %ARG
+ARG="2 4 1 3 5" && ./push_swap $ARG | ./checker %ARG
+ARG="2 4 3 1 5" && ./push_swap $ARG | ./checker %ARG
+
+ARG="3 1 2 4 5" && ./push_swap $ARG | ./checker %ARG
+ARG="3 1 4 2 5" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2 1 4 5" && ./push_swap $ARG | ./checker %ARG
+ARG="3 2 4 1 5" && ./push_swap $ARG | ./checker %ARG
+ARG="3 4 1 2 5" && ./push_swap $ARG | ./checker %ARG
+ARG="3 4 2 1 5" && ./push_swap $ARG | ./checker %ARG
+
+ARG="4 1 2 3 5" && ./push_swap $ARG | ./checker %ARG
+ARG="4 1 3 2 5" && ./push_swap $ARG | ./checker %ARG
+ARG="4 2 1 3 5" && ./push_swap $ARG | ./checker %ARG
+ARG="4 2 3 1 5" && ./push_swap $ARG | ./checker %ARG
+ARG="4 3 1 2 5" && ./push_swap $ARG | ./checker %ARG
+ARG="4 3 2 1 5" && ./push_swap $ARG | ./checker %ARG
+
